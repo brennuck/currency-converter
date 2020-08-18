0b5e3d491b592c2512a08794fb673fb6be245eaa
Brennon Nuckols <brennuck@gmail.com>
2020-08-18 15:01:13 -0600

    progress

--- currency-converter/src/Components/Converter.js
+++ currency-converter/src/Components/Converter.js
@@ -5,46 +5,77 @@ class Converter extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-            rates: [],
-            currency: 0,
-            convertedCurrency: 0
+			rates: [],
+			currency: "",
+			convertedCurrency: "",
 		};
-    }
-    
-    handleChanges = (e) => {
-        this.setState({
-            ...this.state.convertedCurrency,
-            [e.target.name]: e.target.value,
-        });
-    };
-    
-    componentDidMount() {
-        axios.get("http://data.fixer.io/api/latest?access_key=5042badc6b6cc7f6a717ad4cfa088592&format=1")
-        .then(res => {
-            this.setState({ rates: res.data.rates })
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }
+	}
 
-    render() {
-        // console.log(this.state.rates[Object.keys(this.state.rates)[0]]);
-        // console.log(this.state.rates[Object.keys(this.state.rates)[4]]);
-        // console.log(Object.values(this.state.rates))
-        const money = Object.keys(this.state.rates);
-        return (
-            <div>
-                <select>
-                    {money.map((ya) => {
-                        return (
-                            <option> {ya} </option>
-                        )
-                    })}
-                </select>
-            </div>
-        )
-    }
+	handleChanges = (e) => {
+		this.setState({
+			...this.state.currency,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	componentDidMount() {
+		axios
+			.get(
+				"http://data.fixer.io/api/latest?access_key=5042badc6b6cc7f6a717ad4cfa088592&format=1"
+			)
+			.then((res) => {
+				this.setState({ rates: res.data.rates });
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+
+		// const where = this.money.map((ya) => {
+		//     console.log(money.indexOf(ya));
+		//     // return indexOf(ya)
+		// })
+		// return where
+	};
+
+	where() {
+		const money = Object.keys(this.state.rates);
+
+		return (
+			<select>
+				<option>SELECT</option>
+				{money.map((ya) => {
+					return <option> {ya} </option>;
+				})}
+			</select>
+		);
+	}
+
+	render() {
+		// console.log(this.state.rates[Object.keys(this.state.rates)[0]]);
+		// console.log(this.state.rates[Object.keys(this.state.rates)[4]]);
+		// console.log(Object.values(this.state.rates))
+		// const money = Object.keys(this.state.rates);
+		return (
+			<div>
+				<div>{this.where()}</div>
+				<div>
+					<form onSubmit={this.handleSubmit}>
+						<input
+							onChange={this.handleChanges}
+							// value={this.state.currency}
+						/>
+					</form>
+					<button onClick={this.handleSubmit}>
+						W:EOFJOWPEIFJWOPEIJFWf
+					</button>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Converter;
